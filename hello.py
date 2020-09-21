7ecb691a5a2851d533921e58ddca4a8f723c49a6
Alina Mawji <a.mawji08@gmail.com>
2020-09-20 22:47:53 -0500

    edited GET upload_poll

--- hello.py
+++ hello.py
@@ -1,13 +1,31 @@
 from flask import Flask
 app = Flask(__name__)
 
+fake_poll_db = []
+
 @app.route('/')
 def index():
-    return 'Index Page'
+    return 'Index Page 1234'
 
 @app.route('/hello')
 def hello():
     return 'Hello, World'
 
+@app.route('/get_polls')
+def get_polls():
+    for poll in fake_poll_db:
+        log(poll)
+    return 'Successful'
+
+@app.route('/upload_poll/<question>/<answer1>/<answer2>')
+def upload_poll(question, answer1, answer2):
+    poll = {'q': question, 'a1': answer1, 'a2': answer2}
+    fake_poll_db.append(poll)
+    log(fake_poll_db)
+    return '404 Message Not Found'
+
+def log(inpt):
+    print('\t{}'.format(inpt))
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
